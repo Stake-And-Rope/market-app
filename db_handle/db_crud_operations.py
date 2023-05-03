d1b8efe925d906eb2918cdf79fe6619095c966ad
rayapetkova <rayapetkova2007@abv.bg>
2023-05-03 22:30:57 +0300

    Second update crud operations file

--- db_handle/db_crud_operations.py
+++ db_handle/db_crud_operations.py
@@ -9,21 +9,21 @@ def crud_operations():
 
     def create():
         if user_choice == "customers":
-            customer_id = input(f"Enter customer's ID: ")
-            first_name = input(f"\nEnter customer's first name: ")
-            last_name = input(f"\nEnter customer's last name: ")
-            email_address = input(f"\nEnter customer's email address: ")
-            phone = input(f"\nEnter customer's phone number: ")
+            customer_id = input(f"\nEnter customer's ID: ")
+            first_name = input(f"Enter customer's first name: ")
+            last_name = input(f"Enter customer's last name: ")
+            email_address = input(f"Enter customer's email address: ")
+            phone = input(f"Enter customer's phone number: ")
             postgres_conn.admin_client()
             postgres_conn.POSTGRES_CURSOR.execute(f"INSERT INTO customers values ('{customer_id}', '{first_name}', "
                                                   f"'{last_name}', '{email_address}', '{phone}')")
             postgres_conn.POSTGRES_CONNECTION.commit()
         elif user_choice == "employees":
-            employee_id = input(f"Enter employee's ID: ")
-            first_name = input(f"\nEnter employee's first_name: ")
-            last_name = input(f"\nEnter employee's last name: ")
-            email_address = input(f"\nEnter employee's email address: ")
-            phone = input(f"\nEnter employee's phone number: ")
+            employee_id = input(f"\nEnter employee's ID: ")
+            first_name = input(f"Enter employee's first_name: ")
+            last_name = input(f"Enter employee's last name: ")
+            email_address = input(f"Enter employee's email address: ")
+            phone = input(f"Enter employee's phone number: ")
             postgres_conn.admin_client()
             postgres_conn.POSTGRES_CURSOR.execute(f"INSERT INTO employees values ('{employee_id}', '{first_name}', "
                                                   f"'{last_name}', '{email_address}', '{phone}')")
@@ -31,3 +31,9 @@ def crud_operations():
 
     def read():
         pass
+
+    if activity == "create":
+        create()
+
+
+crud_operations()
