462cf1b76be6c9cf43b8ca98b163c925d3f1dc5d
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2023-05-11 21:38:41 +0300

    Portfolio script update

--- db_handle/manage_portfolio.py
+++ db_handle/manage_portfolio.py
@@ -29,7 +29,6 @@ def create_new_category():
     postgres_conn.POSTGRES_CURSOR.execute(f"INSERT INTO categories VALUES ('{category_id}', '{category_name}', '{sub_categories_number[0]}', '{products_number[0]}')")
     postgres_conn.POSTGRES_CONNECTION.commit()
 
-
 def create_new_subcategory():
     """Create ranomly generated sub-category ID"""
     subcategory_id = []
@@ -58,5 +57,50 @@ def create_new_subcategory():
             print(f"Wrong parent category name: {parent_category}. Try again.\n")
             continue
 
+def create_new_items():
+        """Create randomly generated item ID"""
+        item_id = []
+        for i in range(1, 7):
+            item_id.append(random.choice(string.digits))
+        item_id = 'SKU-' + ''.join(item_id)
+        
+        """Enter the item name"""
+        item_name = input("Enter the new item name: ")
+        
+        """Single price"""
+        single_price = float(input("Enter single price: "))
+        
+        """Enter quantity"""
+        quantity = float(input("Enter quantity: "))
+        
+        """Set the item parent and child categories"""
+        while True:
+            parent_category = input("Enter the parent category: ")
+            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT category_name FROM categories WHERE category_name = '{parent_category}'")
+            category_result = postgres_conn.POSTGRES_CURSOR.fetchone()
+            if len(category_result) > 0:
+                while True:
+                    child_category = input("Enter the sub-category: ")
+                    postgres_conn.POSTGRES_CURSOR.execute(f"SELECT subcategory_name FROM subcategories WHERE subcategory_name = '{child_category}'")
+                    subcategory_result = postgres_conn.POSTGRES_CURSOR.fetchone()
+                    if len(subcategory_result) > 0:
+                        postgres_conn.POSTGRES_CURSOR.execute(f"INSERT INTO products (product_id, product_name, category, subcategory, single_price, quantity) VALUES ('{item_id}', '{item_name}',\
+                                                              '{parent_category}', '{child_category}', '{single_price}', '{quantity}')")
+                        postgres_conn.POSTGRES_CURSOR.execute(f"UPDATE subcategories SET total_items = total_items + 1 WHERE subcategory_name = '{child_category}'")
+                        postgres_conn.POSTGRES_CURSOR.execute(f"UPDATE categories SET total_items = total_items + 1 WHERE category_name = '{parent_category}'")
+                        postgres_conn.POSTGRES_CONNECTION.commit()
+                        break
+                    else:
+                        print(f"Child category name {child_category} not exists. Enter another child category.")
+                        continue
+                break
+            else:
+                print(f"Parent category name {parent_category} not exists. Enter another parent category.")
+                continue
+
+        
+        
 # create_new_category()
 # create_new_subcategory()
+
+create_new_items()
