34175e8a836985369f49a3e0abad9b88517acfbd
rayapetkova <rayapetkova2007@abv.bg>
2023-05-13 16:28:01 +0300

    Added new group box
    New group box, new_layout and two buttons "Favourites" and "Log out"

--- pyqt5-gui/main_menu.py
+++ pyqt5-gui/main_menu.py
@@ -65,10 +65,35 @@ class MainMenu(QWidget):
         left_buttons_layout.addSpacing(100)
         left_buttons_groupbox.setLayout(left_buttons_layout)
 
+        """CREATE THE TOP LAYOUT"""
+        top_buttons_groupbox = QGroupBox("Top menu")
+        top_buttons_layout = QHBoxLayout()
+
+        favourites_button = QPushButton()
+        favourites_button.setText("Favourites")
+        favourites_button.setFont(QFont(fonts[0], 9))
+        favourites_button.setFixedWidth(120)
+        favourites_button.setFixedHeight(23)
+
+        log_out_button = QPushButton()
+        log_out_button.setText("Log Out")
+        log_out_button.setFont(QFont(fonts[0], 9))
+        log_out_button.setFixedWidth(100)
+        log_out_button.setFixedHeight(23)
+
+        top_buttons_layout.addWidget(favourites_button)
+        top_buttons_layout.addWidget(log_out_button)
+
+        top_buttons_layout.addStretch(0)
+        top_buttons_layout.addSpacing(1000)
+        top_buttons_groupbox.setLayout(top_buttons_layout)
+
+
         """INIT THE MAIN LAYOUT"""
         main_layout = QGridLayout()
         main_layout.addWidget(user_info_groupbox, 0, 0)
         main_layout.addWidget(left_buttons_groupbox, 1, 0)
+        main_layout.addWidget(top_buttons_groupbox, 0, 1)
 
         """EXAMINE BELOW TWO LINES HOW EXACTLY THEY APPLY THE LOGIC IN THE UI"""
         main_layout.setRowStretch(4, 2)
