8e796b03ef57cb622d0a5c4598b163511e6cac13
techfrapi <techfrapi@gmail.com>
2023-05-17 21:35:15 +0100

    about window created

---
+++ pyqt5-gui/about.py
@@ -0,0 +1,25 @@
+import sys
+
+from PyQt5.QtWidgets import (
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QLabel,
+    QWidget,
+    QApplication
+)
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class About(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        self.setGeometry(650, 300, 500, 200)
+
+
+app = QApplication(sys.argv)
+about_window = About()
+about_window.show()
+app.exec_()
