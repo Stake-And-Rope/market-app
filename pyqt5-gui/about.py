7174527e5c8c98e1b40255a2ff83252673c809d2
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-05-28 11:29:42 +0300

    about_window major GUI rework  - added custom font  - text layouts contents and design rework  - comments updated  - custom window icon  - new vertical layour for github button, current version and release date  - custom button leading to github repository

--- pyqt5-gui/about.py
+++ pyqt5-gui/about.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import sys
 
 from PyQt5.QtWidgets import (
     QVBoxLayout,
@@ -8,23 +7,29 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QLabel,
     QWidget,
-    QApplication
+    QApplication,
 )
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+import sys
+sys.path.append(r'..')
+import webbrowser
 
 class About(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("About")
-        self.setGeometry(650, 300, 600, 400)
+        self.setGeometry(650, 300, 550, 400)
+        self.setWindowIcon(QIcon(r'../img/market.png'))
         
         
         """ADD CUSTOM FONT TO ARRAY READY TO BE LOADED TO ANY TEXT OBJECT"""
         font = QFontDatabase.addApplicationFont(r'../fonts/jetbrains-mono.regular.ttf')
+        font_two = QFontDatabase.addApplicationFont(r'../fonts/JetBrainsMonoNerdFont-Bold.ttf')
         if font >= 0:
             fonts = QFontDatabase.applicationFontFamilies(font)
+            fonts = QFontDatabase.applicationFontFamilies(font_two)
         else:
             print("Error loading fonts!")
 
@@ -32,67 +37,91 @@ class About(QWidget):
 
         """INIT THE OUR STORY LAYOUT"""
         our_story_layout = QVBoxLayout()
+        
         our_story_header = QLabel()
         our_story_header.setText("About MarketApp")
-        our_story_header.setFont(QFont(fonts[0], 9).setBold(True))
+        our_story_header.setFont(QFont(fonts[0], 14, QFont.Bold))
         
-        
-        description_text = "<font size='4'>MarketApp is a hobby-project created by students and professionals,<br>" \
+        description_text = "MarketApp is a hobby-project created by students and professionals,<br>" \
                             "aiming to create an environment for training and gaining new skills:<br>" \
                             "* Python coding skills<br>" \
                             "* Using Python libraries (pyqt5, sshtunnel, python-decouple, etc.)<br>" \
                             "* Linux-based Operating Systems<br>" \
                             "* PostreSQL - Install, Configure and work with database<br>" \
                             "* Qt Framework<br>" \
-                            "* Git/GitHub<>br" \
-                            "* Project Management and Team-work<br>" \
-                            "</font>"
+                            "* Git/GitHub<br>" \
+                            "* Project Management and Team-work<br>"
+                            
         our_story = QLabel()
         our_story.setText(description_text)
-        our_story.setFont(QFont(fonts[0], 9))
+        our_story.setFont(QFont(fonts[0], 10))
 
         our_story_layout.addWidget(our_story_header)
         our_story_layout.addWidget(our_story)
-        our_story_layout.addStretch()
-
-        """CONTACT US LAYOUT"""
-        contact_us_layout = QVBoxLayout()
-        contact_us = QLabel()
-        contact_us.setText("<b><font size='4'>Contact Us</font></b><br>"
-                           "123-456-7890<br>"
-                           "stake_robe@example.com")
-
-        contact_us_layout.addWidget(contact_us)
-        contact_us_layout.addStretch()
+        # our_story_layout.addStretch()
+        our_story_layout.addSpacing(0)
 
         """CONTRIBUTORS LAYOUT"""
         contributors_layout = QVBoxLayout()
 
         contributors_header = QLabel()
-        contributors_header.setText("<b><font size='4'>Contributors</font></b>")
+        contributors_header.setText("Contributors")
+        contributors_header.setFont(QFont(fonts[0], 14, QFont.Bold))
 
         contributors_names = QLabel()
-        contributors_names.setText("<font size='3'>Raya Petkova<br>"
+        contributors_names.setText("Raya Petkova<br>"
                                  "Aleksandar Karastoyanov<br>"
-                                 "Bobby Blagov</font>")
+                                 "Bobby Blagov")
+        contributors_names.setFont(QFont(fonts[0], 10))
 
         contributors = QLabel()
         contributors.setText("Check our GitHub page <a href=\"https://github.com/Stake-And-Rope\">link</a>.")
+        contributors.setFont(QFont(fonts[0], 10))
         contributors.setOpenExternalLinks(True)
 
         contributors_layout.addWidget(contributors_header)
         contributors_layout.addWidget(contributors_names)
-        contributors_layout.addWidget(contributors)
         contributors_layout.addStretch()
+        
+        
+        """GITHUB REPO BUTTON LAYOUT"""
+        github_repo_layout = QVBoxLayout()
+        github_repo_layout.addStretch(0)
+        github_repo_layout.addSpacing(0)
+        
+        github_button = QPushButton()
+        github_button.setText("GitHub Repository")
+        github_button.setFont(QFont(fonts[0], 10))
+        github_button.setStyleSheet("background-color: transparent; \
+                                    border: 0px")
+        github_button.setIcon(QIcon("../img/github.png"))
+        github_button.clicked.connect(lambda: open_github())
+        github_button.setFixedWidth(200)
+        github_button.setFixedHeight(30)
+        
+        current_version = QLabel()
+        current_version.setText("Current Version: 1.0.0")
+        current_version.setFont(QFont(fonts[0], 10))
+        
+        release_date = QLabel()
+        release_date.setText("Release Date: DD-MM-YYYY")
+        release_date.setFont(QFont(fonts[0], 10))
+        
+        github_repo_layout.addWidget(github_button)
+        github_repo_layout.addWidget(current_version)
+        github_repo_layout.addWidget(release_date)
 
 
         """ADDING ALL LAYERS TO MAIN LAYOUT"""
         main_layout.addLayout(our_story_layout)
-        main_layout.addLayout(contact_us_layout)
         main_layout.addLayout(contributors_layout)
+        main_layout.addLayout(github_repo_layout)
 
         self.setLayout(main_layout)
-
+        
+        def open_github():
+            url=str(r'https://github.com/Stake-And-Rope/market-app')
+            webbrowser.open(url)
 
 app = QApplication(sys.argv)
 about_window = About()
