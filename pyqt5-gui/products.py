0fa7a64fa70510bc1caa030a2064264a7a0562cf
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-06-08 00:25:07 +0300

    najor fixes in products:  - function open_products() calls products window  - open_products() inits additional groupbox and replaces it with    subcategories groupbox in the main_layout

--- pyqt5-gui/products.py
+++ pyqt5-gui/products.py
@@ -27,13 +27,14 @@ from collections import deque
 from db_handle import postgres_conn
 
 
-def products_menu(category_name, subcategory_name):
+def products_menu(subcategory_name):
+    app = QApplication(sys.argv)
     products_groupbox = QGroupBox("Products")
     products_grid_layout = QGridLayout()
 
     postgres_conn.admin_client()
 
-    postgres_conn.POSTGRES_CURSOR.execute(f"SELECT product_name FROM products where category = '{category_name}' and subcategory = '{subcategory_name}' ORDER BY product_name ASC;")
+    postgres_conn.POSTGRES_CURSOR.execute(f"SELECT product_name FROM products WHERE subcategory = '{subcategory_name}' ORDER BY product_name ASC;")
 
     result = postgres_conn.POSTGRES_CURSOR.fetchall()
     products_names = deque([p[0] for p in result])
@@ -56,7 +57,11 @@ def products_menu(category_name, subcategory_name):
         current_groupbox.setLayout(current_vertical_layout)
 
         products_grid_layout.addWidget(current_groupbox, 0, col)
-
+    
     products_groupbox.setLayout(products_grid_layout)
+    return products_groupbox
 
-products_menu("Accessories", "Sunglasses")
+    # main_layout.setLayout(products_grid_layout, 1, 1)
+
+# products_menu("Accessories", "Sunglasses")
+this_groupbox = products_menu('Sunglasses')
--- pyqt5-gui/subcategories.py
+++ pyqt5-gui/subcategories.py
@@ -26,6 +26,8 @@ sys.path.append(r'..')
 from collections import deque
 from db_handle import postgres_conn
 # from main_menu_dev import subcategory_name
+from products import products_menu
+
 
 # global subcategory_name
 subcategory_name = ''
@@ -147,49 +149,56 @@ class SubcategoriesMenu(QWidget):
         postgres_conn.POSTGRES_CURSOR.execute(f"SELECT subcategory_name FROM subcategories where parent_category = '{subcategory_name}' ORDER BY subcategory_name ASC;")
         result = postgres_conn.POSTGRES_CURSOR.fetchall()
         subcategories = deque([x[0] for x in result])
-        print(subcategories)
+        subcategories_len = len(subcategories)
+        # print(subcategories)
+        
+        subcats_dict = {
+            'Sunglasses': lambda: open_products('Sunglasses'),
+            'Wallets': lambda: open_products('Wallets'),
+            'Watches': lambda: open_products('Watches')
+        }
 
         for row in range(3):
-            for col in range(4):
-                if subcategories:
-                    current_groupbox = QGroupBox()
-                    current_groupbox.setMaximumWidth(400)
-                    current_groupbox.setMaximumHeight(150)
+            # for col in range(4):
+            # if subcategories:
+            current_groupbox = QGroupBox()
+            current_groupbox.setMaximumWidth(400)
+            current_groupbox.setMaximumHeight(150)
 
-                    current_vertical_layout = QVBoxLayout()
+            current_vertical_layout = QVBoxLayout()
 
-                    subcategory_name = QLabel(f"{subcategories.popleft()}")
-                    subcategory_name.setFont(QFont(fonts[0], 9))
-                    subcat_name_shadow_effect = QGraphicsDropShadowEffect()
-                    subcat_name_shadow_effect.setBlurRadius(2)
-                    subcat_name_shadow_effect.setOffset(1, 1)
-                    subcat_name_shadow_effect.setColor(QColor("blue"))
-                    subcategory_name.setGraphicsEffect(subcat_name_shadow_effect)
-                    subcategory_name.setAlignment(Qt.AlignCenter)
-                    subcategory_name.setStyleSheet("background-color: rgba(255, 255, 255, 255);"
-                                                "border-radius: 10px;")
+            subcategory_name = QLabel(f"{subcategories.popleft()}")
+            subcategory_name.setFont(QFont(fonts[0], 9))
+            subcat_name_shadow_effect = QGraphicsDropShadowEffect()
+            subcat_name_shadow_effect.setBlurRadius(2)
+            subcat_name_shadow_effect.setOffset(1, 1)
+            subcat_name_shadow_effect.setColor(QColor("blue"))
+            subcategory_name.setGraphicsEffect(subcat_name_shadow_effect)
+            subcategory_name.setAlignment(Qt.AlignCenter)
+            subcategory_name.setStyleSheet("background-color: rgba(255, 255, 255, 255);"
+                                        "border-radius: 10px;")
 
-                    image = f"../img/subcategories/{subcategory_name.text()}.png"
-                    groupbox_stylesheet = f"QGroupBox {{ background-image: url({image});" \
-                                          f"border-radius: 10px;" \
-                                          f"}}"
-                    current_groupbox.setStyleSheet(groupbox_stylesheet)
+            image = f"../img/subcategories/{subcategory_name.text()}.png"
+            groupbox_stylesheet = f"QGroupBox {{ background-image: url({image});" \
+                                    f"border-radius: 10px;" \
+                                    f"}}"
+            current_groupbox.setStyleSheet(groupbox_stylesheet)
 
-                    subcategory_button = QPushButton()
-                    subcategory_button.setText(subcategory_name.text())
-                    subcategory_button.setFont(QFont(fonts[0], 11))
-                    subcategory_button.setMaximumWidth(150)
-                    # subcategory_button.setStyleSheet("background-color: rgba(255, 255, 0)")
+            subcategory_button = QPushButton()
+            subcategory_button.setText(subcategory_name.text())
+            subcategory_button.setFont(QFont(fonts[0], 11))
+            subcategory_button.setMaximumWidth(150)
+            subcategory_button.clicked.connect(subcats_dict[subcategory_name.text()])
 
-                    # current_function_name = categories_functions.popleft()
-                    # print(current_function_name)
-                    # category_button.clicked.connect(functions_dict[current_function_name])
 
-                    current_vertical_layout.addWidget(subcategory_button)
+            current_vertical_layout.addWidget(subcategory_button)
 
-                    current_groupbox.setLayout(current_vertical_layout)
-                    subcategories_grid_layout.addWidget(current_groupbox, row, col)
+            current_groupbox.setLayout(current_vertical_layout)
+            subcategories_grid_layout.addWidget(current_groupbox, 0, row)
+        
 
+
+        
         subcategories_groupbox.setLayout(subcategories_grid_layout)
 
         """INIT THE MAIN LAYOUT"""
@@ -198,6 +207,11 @@ class SubcategoriesMenu(QWidget):
         main_layout.addWidget(left_buttons_groupbox, 1, 0)
         main_layout.addWidget(top_buttons_groupbox, 0, 1)
         main_layout.addWidget(subcategories_groupbox, 1, 1)
+
+        
+        def open_products(cat):
+            subcategories_groupbox.hide()
+            main_layout.addWidget(products_menu('Sunglasses'), 1, 1)
 
         """EXAMINE BELOW TWO LINES HOW EXACTLY THEY APPLY THE LOGIC IN THE UI"""
         main_layout.setRowStretch(1, 1)
