0cdd8ab8c77f916b299531541852b64854ba46f0
nikichhh <122815487+nikichhh@users.noreply.github.com>
2023-07-17 11:29:02 +0300

    New Stylsheets

--- pyqt5-gui/login.py
+++ pyqt5-gui/login.py
@@ -34,19 +34,19 @@ class LogIn(QWidget):
         
         title_label = QLabel()
         title_label.setText("Log In")
-        title_label.setFont(QFont(fonts[0], 23))
+        title_label.setFont(QFont("Arial", 23))
         title_label.setAlignment(Qt.AlignCenter)
         
         new_user_label = QLabel()
         new_user_label.setText("New user?")
-        new_user_label.setFont(QFont(fonts[0], 9))
+        new_user_label.setFont(QFont("Arial", 9))
         new_user_label.setAlignment(Qt.AlignCenter)
         new_user_label.setStyleSheet("color: #003366")
         
         create_an_account_button = QPushButton()
         create_an_account_button.clicked.connect(lambda : open_register())
         create_an_account_button.setText("Create an account")
-        create_an_account_button.setFont(QFont(fonts[0], 9))
+        create_an_account_button.setFont(QFont("Arial", 9))
         create_an_account_button.setFlat(True)
         
         title_layout.addWidget(title_label)
@@ -56,32 +56,57 @@ class LogIn(QWidget):
         """Init the first center vertical layout"""
         first_center_vertical_layout = QVBoxLayout()
 
-        username_label = QLabel()
-        username_label.setText("Username")
-        username_label.setFont(QFont(fonts[0], 12))
+        username_label = QLineEdit()
+        username_label.setPlaceholderText("Username")
+        username_label.setStyleSheet("""
+            QLineEdit {
+                width: 100%;
+                height: 40px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                margin-bottom: 10px;
+            }
+        """)
 
-        username_textbox = QLineEdit()
-        username_textbox.setFont(QFont(fonts[0], 11))
-
-        password_label = QLabel()
-        password_label.setText("Password")
-        password_label.setFont(QFont(fonts[0], 12))
-
-        password_textbox = QLineEdit()
-        password_textbox.setFont(QFont(fonts[0], 11))
-        password_textbox.setEchoMode(QLineEdit.Password)
+        password_label = QLineEdit()
+        password_label.setPlaceholderText("Password")
+        password_label.setEchoMode(QLineEdit.Password)
+        password_label.setStyleSheet("""
+            QLineEdit {
+                width: 100%;
+                height: 40px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+            }
+        """)
         
-        log_in_button = QPushButton()
+        log_in_button = QPushButton("Log in")
         log_in_button.clicked.connect(lambda : login())
-        log_in_button.setText("Log in")
-        log_in_button.setFont(QFont(fonts[0], 11))
-        log_in_button.setStyleSheet("background-color : lightBlue")
+        log_in_button.setStyleSheet("""
+            QPushButton {
+                width: 100%;
+                height: 40px;
+                font-size: 14px;
+                font-weight: bold;
+                background-color: #3ca9e2;
+                color: #fff;
+                border: none;
+                border-radius: 4px;
+                margin-top: 10px;
+                padding: 0;
+            }
+            QPushButton:hover {
+                background-color: #329dd5;
+            }
+        """)
 
         """Add labels to the center vertical layout"""
         first_center_vertical_layout.addWidget(username_label)
-        first_center_vertical_layout.addWidget(username_textbox)
         first_center_vertical_layout.addWidget(password_label)
-        first_center_vertical_layout.addWidget(password_textbox)
         first_center_vertical_layout.addWidget(log_in_button)
 
         """Init the Main Layout"""
@@ -93,7 +118,7 @@ class LogIn(QWidget):
         
         def login():
             try:
-                postgres_conn.customer_client(username_textbox.text(), password_textbox.text())
+                postgres_conn.customer_client(username_label.text(), password_label.text())
             except (Exception) as error:
                 error_msg_box = QMessageBox(self)
                 error_msg_box.setIcon(QMessageBox.Warning)
--- pyqt5-gui/register.py
+++ pyqt5-gui/register.py
@@ -20,6 +20,7 @@ from db_handle import postgres_conn, register_user
 import login
 import random, re, string
 
+
 """Create the QWidget class and initiate the objects inside"""
 class Register(QWidget):
     def __init__(self):
@@ -57,179 +58,179 @@ class Register(QWidget):
         
         qlabels_list = []
         qlineedit_list = []
-        
-        """Init the left vertical layout, containing the qlabels_list objects"""
+
+        """Init the labels, containing textboxes in them"""
+
         left_vertical_layout = QVBoxLayout()
         left_vertical_layout.addStretch()
         left_vertical_layout.addSpacing(2)
 
-        user_name_label = QLabel()
-        user_name_label.setText("User Name")
-        user_name_label.setFont(QFont(fonts[0], 12))
-        user_name_label.setFixedHeight(25)
-        user_name_label.setAlignment(Qt.AlignLeft)
-        user_name_label.setStyleSheet("color: #003366")
-        qlabels_list.append(user_name_label)   
-    
-        first_name_label = QLabel()
-        first_name_label.setText("First Name")
-        first_name_label.setFont(QFont(fonts[0], 12))
-        first_name_label.setFixedHeight(25)
-        first_name_label.setAlignment(Qt.AlignLeft)
-        first_name_label.setStyleSheet("color: #003366")
-        qlabels_list.append(first_name_label)
+        form_layout = QVBoxLayout()
 
-        last_name_label = QLabel()
-        last_name_label.setText("Last Name")
-        last_name_label.setFont(QFont(fonts[0], 12))
-        last_name_label.setFixedHeight(25)
-        last_name_label.setAlignment(Qt.AlignLeft)
-        last_name_label.setStyleSheet("color: #003366")
-        qlabels_list.append(last_name_label)
+        name_layout = QHBoxLayout()
 
-        email_address_label = QLabel()
-        email_address_label.setText("Email Address")
-        email_address_label.setFont(QFont(fonts[0], 12))
-        email_address_label.setFixedHeight(25)
-        email_address_label.setAlignment(Qt.AlignLeft)
-        email_address_label.setStyleSheet("color: #003366")
-        qlabels_list.append(email_address_label)
+        first_name_label = QLineEdit()
+        first_name_label.setPlaceholderText("First name")
+        first_name_label.setStyleSheet("""
+                    QLineEdit {
+                        width: 180px;
+                        height: 30px;
+                        font-size: 16px;
+                        padding: 5px;
+                        border: 1px solid #ccc;
+                        border-radius: 4px;
+                        margin-right: 10px;
+                    }
+                """)
+        name_layout.addWidget(first_name_label)
 
-        phone_number_label = QLabel()
-        phone_number_label.setText("Phone Number")
-        phone_number_label.setFont(QFont(fonts[0], 12))
-        phone_number_label.setFixedHeight(25)
-        phone_number_label.setAlignment(Qt.AlignLeft)
-        phone_number_label.setStyleSheet("color: #003366")
-        qlabels_list.append(phone_number_label)
+        last_name_label = QLineEdit()
+        last_name_label.setPlaceholderText("Last name")
+        last_name_label.setStyleSheet("""
+                    QLineEdit {
+                        width: 180px;
+                        height: 30px;
+                        font-size: 16px;
+                        padding: 5px;
+                        border: 1px solid #ccc;
+                        border-radius: 4px;
+                    }
+                """)
+        name_layout.addWidget(last_name_label)
+
+        form_layout.addLayout(name_layout)
+
+        user_name_label = QLineEdit()
+        user_name_label.setPlaceholderText("Username")
+        user_name_label.setStyleSheet("""
+            QLineEdit {
+                width: 370px;
+                height: 30px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                margin-bottom: 10px;
+            }
+        """)
+        form_layout.addWidget(user_name_label)
+
+        phone_number_label = QLineEdit()
+        phone_number_label.setPlaceholderText("Phone Number")
+        phone_number_label.setStyleSheet("""
+            QLineEdit {
+                width: 370px;
+                height: 30px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                margin-bottom: 10px;
+            }
+        """)
+        form_layout.addWidget(phone_number_label)
+
+        email_address_label = QLineEdit()
+        email_address_label.setPlaceholderText("Email Address")
+        email_address_label.setStyleSheet("""
+            QLineEdit {
+            
+                width: 370px;
+                height: 30px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                margin-bottom: 10px;
+            }
+        """)
+        form_layout.addWidget(email_address_label)
         
         """Password Label is initialized two times"""
-        password_label = QLabel()
-        password_label.setText("Password")
-        password_label.setFont(QFont(fonts[0], 12))
-        password_label.setFixedHeight(25)
-        password_label.setAlignment(Qt.AlignLeft)
-        password_label.setStyleSheet("color: #003366")
-        qlabels_list.append(password_label)
-        password_label_repeat = QLabel()
-        password_label_repeat.setText("Repeat Password")
-        password_label_repeat.setFont(QFont(fonts[0], 12))
-        password_label_repeat.setFixedHeight(25)
-        password_label_repeat.setAlignment(Qt.AlignLeft)
-        password_label_repeat.setStyleSheet("color: #003366")
-        qlabels_list.append(password_label_repeat)
 
-        """Apply shadow effect to all QLabel added to qlabels_list array
-            shadow variable is customizable"""
-        for i in range(len(qlabels_list)):
-            shadow = QGraphicsDropShadowEffect()
-            shadow.setOffset(2, 1)
-            shadow.setColor(QColor(255, 255, 255))
-            qlabels_list[i].setGraphicsEffect(shadow)
+        password_label = QLineEdit()
+        password_label.setPlaceholderText("Password")
+        password_label.setEchoMode(QLineEdit.Password)
+        password_label.setStyleSheet("""
+            QLineEdit {
+                width: 370px;
+                height: 30px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                margin-bottom: 10px;
+            }
+        """)
+        form_layout.addWidget(password_label)
 
-        """Add the objects to the left vertical objects"""
-        left_vertical_layout.addWidget(user_name_label)
-        left_vertical_layout.addWidget(first_name_label)
-        left_vertical_layout.addWidget(last_name_label)
-        left_vertical_layout.addWidget(phone_number_label)
-        left_vertical_layout.addWidget(email_address_label)
-        left_vertical_layout.addWidget(password_label)
-        left_vertical_layout.addWidget(password_label_repeat)
-
-        """Init the right vertical layout, containing the QLineEdit objects"""
-        right_vertical_layout = QVBoxLayout()
-        right_vertical_layout.addStretch()
-        right_vertical_layout.addSpacing(2)
-        
-        user_name_textbox = QLineEdit()
-        user_name_textbox.setFont(QFont(fonts[0], 12))
-        user_name_textbox.setFixedWidth(300)
-        user_name_textbox.setFixedHeight(25)
-        user_name_textbox.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(user_name_textbox)
-        
-        first_name_textbox = QLineEdit()
-        first_name_textbox.setFont(QFont(fonts[0], 12))
-        first_name_textbox.setFixedWidth(300)
-        first_name_textbox.setFixedHeight(25)
-        first_name_textbox.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(first_name_textbox)
-
-        last_name_textbox = QLineEdit()
-        last_name_textbox.setFont(QFont(fonts[0], 12))
-        last_name_textbox.setFixedWidth(300)
-        last_name_textbox.setFixedHeight(25)
-        last_name_textbox.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(last_name_textbox)
-
-        email_address_textbox = QLineEdit()
-        email_address_textbox.setFont(QFont(fonts[0], 12))
-        email_address_textbox.setFixedWidth(300)
-        email_address_textbox.setFixedHeight(25)
-        email_address_textbox.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(email_address_textbox)
-        
-        phone_number_textbox = QLineEdit()
-        phone_number_textbox.setFont(QFont(fonts[0], 12))
-        phone_number_textbox.setFixedWidth(300)
-        phone_number_textbox.setFixedHeight(25)
-        phone_number_textbox.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(phone_number_textbox)
-
-        password_textbox = QLineEdit()
-        password_textbox.setEchoMode(QLineEdit.Password)
-        password_textbox.setFixedWidth(300)
-        password_textbox.setFixedHeight(25)
-        password_textbox.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(password_textbox)
-        password_textbox_repeat = QLineEdit()
-        password_textbox_repeat.setEchoMode(QLineEdit.Password)
-        password_textbox_repeat.setFixedWidth(300)
-        password_textbox_repeat.setFixedHeight(25)
-        password_textbox_repeat.setAlignment(Qt.AlignLeft)
-        qlineedit_list.append(password_textbox_repeat)
-        
-        """Apply shadow effect to qlabels_list array
-            shadow variable is customizable"""
-        for i in range(len(qlineedit_list)):
-            shadow = QGraphicsDropShadowEffect()
-            shadow.setOffset(2, 1)
-            shadow.setColor(QColor(0, 51, 102))
-            qlineedit_list[i].setGraphicsEffect(shadow)
-            
-        """Add the object to the right vertical layout"""
-        right_vertical_layout.addWidget(user_name_textbox)
-        right_vertical_layout.addWidget(first_name_textbox)
-        right_vertical_layout.addWidget(last_name_textbox)
-        right_vertical_layout.addWidget(phone_number_textbox)
-        right_vertical_layout.addWidget(email_address_textbox)
-        right_vertical_layout.addWidget(password_textbox)
-        right_vertical_layout.addWidget(password_textbox_repeat)
-
-        """Add right and left layout to the main horizontal layout"""
-        main_horizontal_layout.addLayout(left_vertical_layout)
-        main_horizontal_layout.addLayout(right_vertical_layout)
+        password_label_repeat = QLineEdit()
+        password_label_repeat.setPlaceholderText("Repeat Password")
+        password_label_repeat.setEchoMode(QLineEdit.Password)
+        password_label_repeat.setStyleSheet("""
+            QLineEdit {
+                width: 370px;
+                height: 30px;
+                font-size: 16px;
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+            }
+        """)
+        form_layout.addWidget(password_label_repeat)
         
         """Buttons Layout"""
         buttons_layout = QHBoxLayout()
-        buttons_layout.addStretch()
-        buttons_layout.addSpacing(2)
 
         register_button = QPushButton()
+        register_button.setGeometry(50, 50, 200, 50)
+        register_button.setStyleSheet("""
+                            QPushButton {
+                                width: 185px;
+                                height: 30px;
+                                font-size: 14px;
+                                font-weight: bold;
+                                background-color: #3ca9e2;
+                                color: #fff;
+                                border: none;
+                                border-radius: 4px;
+                                margin-top: 10px;
+                            }
+                            QPushButton:hover {
+                                background-color: #329dd5;
+                            }
+                        """)
         register_button.clicked.connect(lambda : create_new_account())
         register_button.setText('Register')
-        register_button.setFont(QFont(fonts[0], 12))
+        register_button.setFont(QFont("Arial", 12))
 
         back_button = QPushButton()
         back_button.clicked.connect(lambda: open_login())
         back_button.setText('Back')
-        back_button.setFont(QFont(fonts[0], 12))
+        back_button.setFont(QFont("Arial", 12))
+        back_button.setStyleSheet("""
+                            QPushButton {
+                                width: 185px;
+                                height: 30px;
+                                font-size: 14px;
+                                font-weight: bold;
+                                background-color: #ccc;
+                                color: #fff;
+                                border: none;
+                                border-radius: 4px;
+                                margin-top: 10px;
+                            }
+                            QPushButton:hover {
+                                background-color: #999;
+                            }
+                        """)
 
         buttons_layout.addWidget(register_button)
         buttons_layout.addWidget(back_button)
 
         """Init the Main Layout which loads all objects above"""
         main_layout = QVBoxLayout()
+        main_layout.addLayout(form_layout)
         main_layout.addLayout(main_horizontal_layout)
         main_layout.addLayout(buttons_layout)
         main_layout.addLayout(image_layout)
@@ -249,39 +250,39 @@ class Register(QWidget):
                 if user_id not in current_ids:
                     break
             
-            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT username FROM customers WHERE username = '{user_name_textbox.text()}'")
+            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT username FROM customers WHERE username = '{user_name_label.text()}'")
             if postgres_conn.POSTGRES_CURSOR.fetchall():
                 create_account_errors.append('Username already exists')
             
             email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-            if not re.match(email_pattern, email_address_textbox.text()):
+            if not re.match(email_pattern, email_address_label.text()):
                 create_account_errors.append('Incorect email address')
 
-            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT email_address FROM customers WHERE email_address = '{email_address_textbox.text()}'")
+            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT email_address FROM customers WHERE email_address = '{email_address_label.text()}'")
             if postgres_conn.POSTGRES_CURSOR.fetchall():
                 create_account_errors.append('Email address already existing')
 
-            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT phone FROM customers WHERE phone = '{phone_number_textbox.text()}'")
+            postgres_conn.POSTGRES_CURSOR.execute(f"SELECT phone FROM customers WHERE phone = '{phone_number_label.text()}'")
             if postgres_conn.POSTGRES_CURSOR.fetchall():
                 create_account_errors.append('Phone number already exists')
                 
-            if password_textbox.text() != password_textbox_repeat.text():
+            if password_label.text() != password_label_repeat.text():
                 create_account_errors.append('Password fields does not match.')
-            if len(password_textbox.text()) < 8:
+            if len(password_label.text()) < 8:
                 create_account_errors.append('Password is too short. It must be at least 8 characters.')
-            if not re.search(re.compile('[!@#$%^&*()-+?_=,<>/]'), password_textbox.text()):
+            if not re.search(re.compile('[!@#$%^&*()-+?_=,<>/]'), password_label.text()):
                 create_account_errors.append("Password must contain at least one special character.")
-            if not re.search(re.compile('[A-Z]'), password_textbox.text()):
+            if not re.search(re.compile('[A-Z]'), password_label.text()):
                 create_account_errors.append("Password must contain at least one uppercase letter.")
-            if not re.search(re.compile('[0-9]'), password_textbox.text()):
+            if not re.search(re.compile('[0-9]'), password_label.text()):
                 create_account_errors.append("Password must contain at least one number.")
             
             if len(create_account_errors) == 0:
                     """Create new record inside customers table"""
                     postgres_conn.POSTGRES_CURSOR.execute(f"INSERT INTO customers (customer_id, username, first_name, last_name, email_address, phone) \
-                                                          VALUES ({user_id}, '{user_name_textbox.text().lower()}', '{first_name_textbox.text()}', '{last_name_textbox.text()}',\
-                                                              '{email_address_textbox.text()}', '{phone_number_textbox.text()}')")
-                    register_user.create_user(user_name_textbox.text(), password_textbox.text())
+                                                          VALUES ({user_id}, '{user_name_label.text().lower()}', '{first_name_label.text()}', '{last_name_label.text()}',\
+                                                              '{email_address_label.text()}', '{phone_number_label.text()}')")
+                    register_user.create_user(user_name_label.text(), password_label.text())
             else:
                 error_msg_box = QMessageBox(self)
                 error_msg_box.setIcon(QMessageBox.Warning)
