1e11d05a0c04fa89fd59447c51b1276110983550
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-08-06 22:42:57 +0300

    register_user new logic

--- db_handle/register_user.py
+++ db_handle/register_user.py
@@ -3,13 +3,15 @@ sys.path.append(r'.')
 from db_handle import postgres_conn
 
 # postgres_conn.admin_client()
+admin_cursor = postgres_conn.POSTGRES_CURSOR
+admin_connection = postgres_conn.POSTGRES_CONNECTION
 
 """Create new user in pg_user table and grant him customers_marketapp role with privileges to read and update tables on the database(only public schemas)."""
 def create_user(username, passwd):
     try:
-        postgres_conn.POSTGRES_CURSOR.execute(f"CREATE USER {username}_marketapp WITH PASSWORD '{passwd}'") # <--{username}_marketapp will distinguish the users for market-app DB from the rest of the users
-        postgres_conn.POSTGRES_CURSOR.execute(f"GRANT customers_marketapp TO {username}_marketapp")  
-        postgres_conn.POSTGRES_CONNECTION.commit()
+        admin_cursor.execute(f"CREATE USER {username}_marketapp WITH PASSWORD '{passwd}'") # <--{username}_marketapp will distinguish the users for market-app DB from the rest of the users
+        admin_cursor.execute(f"GRANT customers_marketapp TO {username}_marketapp")  
+        admin_connection.commit()
     except (Exception) as error:
         print("Error creating new user")
     
