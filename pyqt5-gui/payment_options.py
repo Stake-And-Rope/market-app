dec1a93b7d81c03c6fc54b6c78b3d93dd4a82eaf
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-08-25 21:58:16 +0300

    payment options menu reworked

--- pyqt5-gui/payment_options.py
+++ pyqt5-gui/payment_options.py
@@ -19,9 +19,11 @@ from PyQt5.QtWidgets import (QApplication,
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
+from datetime import datetime
 sys.path.append(r'..')
 from collections import deque
 from db_handle import postgres_conn
+import login
 
 """ADMIN CLIENT TO THE POSTGRE DATABASE"""
 admin_cursor = postgres_conn.POSTGRES_CURSOR
@@ -32,49 +34,85 @@ user_cursor = postgres_conn.USER_POSTGRES_CURSOR
 user_connection = postgres_conn.USER_POSTGRES_CONNECTION
 
 def open_payment_options():
+    login.user_cursor.execute("SELECT current_user")
+    global current_user
+    current_user = login.user_cursor.fetchone()
+    current_user = current_user[0].replace("_marketapp", "")
+    
     """ADD CUSTOM FONT TO ARRAY READY TO BE LOADED TO ANY TEXT OBJECT"""
     font = QFontDatabase.addApplicationFont(r'../fonts/jetbrains-mono.regular.ttf')
     if font < 0:
         print('Error loading fonts!')
     fonts = QFontDatabase.applicationFontFamilies(font)
     
-    # change the query to dinamyc in production
-    admin_cursor.execute(f"SELECT * FROM payment_options WHERE username = 'pesho'")
+    admin_cursor.execute(f"SELECT * FROM payment_options WHERE username = '{current_user}'")
     result = admin_cursor.fetchall()
+    print(result)
     len_result = len(result)
 
     global payment_options_groupbox
-    payment_options_groupbox = QGroupBox("Payment Options")
+    payment_options_groupbox = QGroupBox()
+    payment_options_layout = QVBoxLayout()
+    payment_options_layout.addStretch()
+    payment_options_layout.addSpacing(5)
     payment_options_groupbox.setAlignment(Qt.AlignLeft)
-
-
-    main_payment_layout = QHBoxLayout()
-    main_payment_layout.addStretch()
-    main_payment_layout.addSpacing(10)
-    main_payment_layout.setAlignment(Qt.AlignCenter)
     
-    for i in result:
-        current_payment_option = QVBoxLayout()
-        current_payment_option.setAlignment(Qt.AlignRight)
-        text_labels = deque(['Payment Code', 'Payment Name', 'Payment Type', 'Card Number', 'Card Holder', 'CCV', 'Expire Date', 'Default', 'Username'])
-        for j in range(len(i)):
-            current_line = QHBoxLayout()
-            current_line.addStretch()
-            current_line.addSpacing(2)
-            left_side = QLabel(text_labels.popleft() + ": ")
-            left_side.setFont(QFont(fonts[0], 12))
-            left_side.setAlignment(Qt.AlignCenter)
-            
-            right_side = QLabel(str(i[j]))
-            right_side.setAlignment(Qt.AlignCenter)
-            
-            current_line.addWidget(left_side)
-            current_line.addWidget(right_side)
-            
-            current_payment_option.addLayout(current_line)
-        main_payment_layout.addLayout(current_payment_option)
-            
-    payment_options_groupbox.setLayout(main_payment_layout)
+    for i in range(len(result)):
+        current_payment_option = QHBoxLayout()
+        current_payment_option.setAlignment(Qt.AlignLeft)
+        
+        card_type = result[i][2]
+        
+        payment_type_label = QLabel()
+        if card_type == "Visa":
+            payment_type_label.setProperty("class", "payment_label_visa")
+        elif card_type == "MasterCard":
+            payment_type_label.setProperty("class", "payment_label_mastercard")
+        elif card_type == "Revolut":
+            payment_type_label.setProperty("class", "payment_label_revolut")
+        payment_type_label.setFixedSize(64, 64)
+        
+        payment_name = QLabel()
+        payment_name.setText(f'Payment Name: {result[i][1]} /')
+        payment_name.setFont(QFont(fonts[0], 12))
+        
+        card_holder = QLabel()
+        card_holder.setText(f'Card Holder: {result[i][4]} /')
+        card_holder.setFont(QFont(fonts[0], 12))
+        
+        card_number = QLabel()
+        card_number.setText(f'Card Number: {result[i][3]} /')
+        card_number.setFont(QFont(fonts[0], 12))
+        
+        date_label = QLabel()
+        date_label.setText(f'Valid until: {str(result[i][6])}')
+        
+        delete_button = QPushButton()
+        delete_button.setIcon(QIcon(r"../img/trash.png"))
+        delete_button.setIconSize(QSize(32, 32))
+        delete_button.setToolTip("Delete Card")
+
+        
+        current_payment_option.addWidget(payment_type_label)
+        current_payment_option.addWidget(payment_name)
+        current_payment_option.addWidget(card_holder)
+        current_payment_option.addWidget(card_number)
+        current_payment_option.addWidget(date_label)
+        current_payment_option.addWidget(delete_button)
+
+    
+        payment_options_layout.addLayout(current_payment_option)
+    
+    
+    add_new_card = QPushButton()
+    add_new_card.setText("Add New Card")
+    add_new_card.setFont(QFont(fonts[0], 12))
+    add_new_card.setFixedSize(150, 30)
+    
+    payment_options_layout.addWidget(add_new_card)
+    
+    payment_options_groupbox.setLayout(payment_options_layout)
+
     
 
     return payment_options_groupbox
